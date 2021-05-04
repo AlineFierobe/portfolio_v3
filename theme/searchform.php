1b0207830423c28508780c3d3f8272a63e4a4918
Aline Fierobe <contact@alinefierobe.com>
2021-05-04 09:29:06 +0200

    delete unuse files

--- theme/searchform.php
+++
@@ -1,7 +0,0 @@
-<form method="get" id="form" action="<?php bloginfo('url'); ?>/">
-    <input type="text" value="<?php the_search_query(); ?>" name="s" id="s">
-    <input type="submit" id="submit" value="search">
-</form>
-
-<!-- CODE POUR APPELER LE SEARCHFORM -->
-<?php get_search_form(); ?>
